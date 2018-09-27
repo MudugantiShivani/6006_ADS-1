961f0988c335f02e7e1381b136004c78b825bb3e
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-09-27 19:35:02 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1-Assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ ADS-1-Assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -1,64 +1,115 @@
 import java.util.Scanner;
-class stringstack {
-	private Node first = null;
-	private class Node {
-		char item;
-		Node nextAddress;
-	}
-	public char top() {
-		return first.item;
-	}
+/**
+ * Class for stringstack.
+ */
+class Stringstack {
+    /**
+     * { var_description }.
+     */
+    private Node first = null;
+    /**
+     * Class for node.
+     */
+    private class Node {
+        private char item;
+        private Node nextAddress;
+    }
+    /**
+     * { function_description }.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public char top() {
+        return first.item;
+    }
+    /**
+     * Determines if empty.
+     *
+     * @return     True if empty, False otherwise.
+     */
 
-
-	public boolean isEmpty() {
-		return first == null;
-	}
-	public void push(char item) {
-		Node oldfirst = first;
-		first = new Node();
-		first.item = item;
-		first.nextAddress = oldfirst;
-	}
-	public char pop() {
-		char item = first.item;
-		first = first.nextAddress;
-		return item;
-	}
+    public boolean isEmpty() {
+        return first == null;
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      item  The item
+     */
+    public void push(final char item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.nextAddress = oldfirst;
+    }
+    /**
+     * { function_description }.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public char pop() {
+        char item = first.item;
+        first = first.nextAddress;
+        return item;
+    }
 }
+/**
+ * Class for solution.
+ */
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int size = Integer.parseInt(sc.nextLine());
-		for (int i = 0; i < size; i++) {
-			String line = sc.next();
-			if(parenthesisfile(line)){
-				System.out.println("YES");
-			} else {
-				System.out.println("NO");
-			}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        /**
+         * { item_description }.
+         */
 
-		}
-	}
-	public static boolean parenthesisfile(String s) {
-		stringstack obj = new stringstack();
-		int sLength = s.length();
-		for (int i = 0; i < sLength; i++) {
-			char ch = s.charAt(i);
-			if (ch == '(' || ch == '{' || ch == '[') {
-				obj.push(ch);
-			}
-			else {
-				if(obj.isEmpty()) return false;
-				if (ch == ')' && obj.top() == '(') {
-				obj.pop();
-			} else if (ch == '}' && obj.top() == '{') {
-				obj.pop();
-			} else if (ch == ']' && obj.top() == '[') {
-				obj.pop();
-			} else return false;
-		}
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < size; i++) {
+            String line = sc.next();
+            if (parenthesisfile(line)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
 
-	}
-	return obj.isEmpty();
+        }
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      s     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static boolean parenthesisfile(final String s) {
+        Stringstack obj = new Stringstack();
+        int sLength = s.length();
+        for (int i = 0; i < sLength; i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                obj.push(ch);
+            } else {
+                if(obj.isEmpty()) return false;
+                if (ch == ')' && obj.top() == '(') {
+                obj.pop();
+            } else if (ch == '}' && obj.top() == '{') {
+                obj.pop();
+            } else if (ch == ']' && obj.top() == '[') {
+                obj.pop();
+            } else return false;
+        }
+
+    }
+    return obj.isEmpty();
 }
 }
