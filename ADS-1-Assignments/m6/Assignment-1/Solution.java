8cce93d96d02ba55e128c5230ebe4f6b2d039bca
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-09-29 12:21:34 +0530

    "check_style_score -> 6 of 12 passed. style score: 0/1 "

--- ADS-1-Assignments/m6/Assignment-1/Solution.java
+++ ADS-1-Assignments/m6/Assignment-1/Solution.java
@@ -32,7 +32,7 @@ class AddLargeNumbers {
                 result += carry;
                 carry = result/10;
                 result %= 10;
-                objct1.insertStart(result);
+                objct1.insertEnd(result);
             }
         }
         return objct1;
