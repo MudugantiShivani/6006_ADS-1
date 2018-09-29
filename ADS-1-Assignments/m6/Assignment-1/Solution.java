f3dbc9894c508a2086221c05165f6af40439663d
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-09-29 12:13:39 +0530

    "check_style_score -> 6 of 12 passed. style score: 0/1 "

--- ADS-1-Assignments/m6/Assignment-1/Solution.java
+++ ADS-1-Assignments/m6/Assignment-1/Solution.java
@@ -24,7 +24,7 @@ class AddLargeNumbers {
         int result = 0;
         while (list1.size != 0 && list2.size != 0) {
             result = list1.deleteStart() + list2.deleteStart() + result;
-            objct1.insertEnd((result));
+            objct1.insertStart((result));
         }
         return objct1;
     }
