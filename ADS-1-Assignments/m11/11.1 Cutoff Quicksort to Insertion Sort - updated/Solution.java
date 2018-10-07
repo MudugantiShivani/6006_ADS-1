b561c41db3e760e38ac26104c8df4f3d869461d5
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-10-07 22:49:35 +0530

    "ads1_m11_a1_v2: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-Assignments/m11/11.1 Cutoff Quicksort to Insertion Sort - updated/Solution.java
+++ ADS-1-Assignments/m11/11.1 Cutoff Quicksort to Insertion Sort - updated/Solution.java
@@ -1,19 +1,49 @@
 import java.util.Scanner;
+/**
+ * Class for quick.
+ */
 class Quick {
     private int cutoff;
+    /**
+     * Constructs the object.
+     *
+     * @param      cutof  The cutof
+     */
     Quick(final int cutof) {
         this.cutoff = cutof;
     }
+    /**
+     * { compqres the numbers }.
+     *
+     * @param      a     { one parameter }
+     * @param      b     { second parameter }
+     *
+     * @return     { returns the boolean value }
+     */
     public boolean lesser(
         final Comparable a, final Comparable b) {
         return a.compareTo(b) < 0;
     }
+    /**
+     * { used for swapping}.
+     *
+     * @param      array  The array
+     * @param      i      { one element }
+     * @param      j      { second element }
+     */
     public void swap(
         final Comparable[] array, final int i, final int j) {
         Comparable temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
+    /**
+     * { function for insertion sort }.
+     *
+     * @param      array  The array
+     * @param      low    The low
+     * @param      high   The high
+     */
     public void insertionSort(
         final Comparable[] array, final int low, final int high) {
         for (int i = low; i <= high; i++) {
@@ -22,6 +52,13 @@ class Quick {
             }
         }
     }
+    /**
+     * { function calls when insertion sort is called }.
+     *
+     * @param      array  The array
+     * @param      low    The low
+     * @param      high   The high
+     */
     public void sort(
         final Comparable[] array, final int low, final int high) {
         if (high - low + 1  <=  cutoff) {
@@ -33,6 +70,15 @@ class Quick {
         sort(array, low, j - 1);
         sort(array, j + 1, high);
     }
+    /**
+     * { parttions the elements }.
+     *
+     * @param      array  The array
+     * @param      low    The low
+     * @param      high   The high
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int partition(
         final Comparable[] array, final int low, final int high) {
         int start = low;
@@ -58,6 +104,13 @@ class Quick {
         System.out.println(print(array));
         return end;
     }
+    /**
+     * { prints the output as string }.
+     *
+     * @param      arr   The arr
+     *
+     * @return     { returns the string }
+     */
     public String print(final Comparable[] arr) {
         String str = "[";
         int i;
@@ -68,9 +121,20 @@ class Quick {
         return str;
     }
 }
+/**
+ * { main class }.
+ */
 final class Solution {
+    /**
+     * Constructs the object.
+     */
     private Solution() {
     }
+    /**
+     * { main  function}.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int inputs = scan.nextInt();
