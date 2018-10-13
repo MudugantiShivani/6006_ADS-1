323baf9b0a0fd482f13538a7efc7f28d9b9f90bf
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-10-13 12:29:50 +0530

    "ads1_week3_exam: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-Assignments/m18/Week-3_Assignment-1/Solution.java
+++ ADS-1-Assignments/m18/Week-3_Assignment-1/Solution.java
@@ -4,47 +4,49 @@ class Solution{
 	}
 	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
-		StockExchange obj = new StockExchange();
 		int range = scan.nextInt();
-		int numberOfhours = 6;
+		int numberof_hours = 6;
 		scan.nextLine();
-		while(numberOfhours > 0){
+		while(numberof_hours > 0){
+			StockOrder obj = new StockOrder();
 			for(int i = 0; i < range; i++) {
 				String line = scan.nextLine();
 				String[] tokens = line.split(",");
 				obj.put(tokens[0], tokens[1]);
 			}
 			obj.print();
-			numberOfhours--;
+			numberof_hours--;
 		}
 		int next = scan.nextInt();
 	}
 }
-class StockExchange {
-	BST<Double, String> MaxObj;
-	BST<Double, String> MinObj;
-	StockExchange() {
-		MaxObj = new BST<Double, String>();
-		MinObj = new BST<Double, String>();
+class StockOrder{
+	BST<Double, String> maxObj;
+	BST<Double, String> minObj;
+	StockOrder(){
+		maxObj = new BST<Double, String>();
+		minObj = new BST<Double, String>();
 	}
-	public void put(String name, String percentage) {
-		MaxObj.put(Double.parseDouble(percentage), name);
-		MinObj.put(Double.parseDouble(percentage), name);
+	public void put(String name, String percent){
+		maxObj.put(Double.parseDouble(percent), name);
+		minObj.put(Double.parseDouble(percent), name);
 	}
 	public void print(){
 		for (int i = 0; i < 5; i++) {
-			Double min = MinObj.min();
-			String minName = MinObj.get(min);
-			MinObj.deleteMin();
-			System.out.println(minName + " "+ min);
+			Double temporarymin = minObj.min();
+			String minName = minObj.get(temporarymin);
+			minObj.deleteMin();
+			System.out.println(minName + " "+ temporarymin);
 		}
 		System.out.println();
 		for (int i = 0; i < 5; i++) {
-			Double tempMax = MaxObj.max();
-			String nameMax = MaxObj.get(tempMax);
-			MaxObj.deleteMax();
-			System.out.println(nameMax + " "+ tempMax);
+			Double temporarymax = maxObj.max();
+			String maxName = maxObj.get(temporarymax);
+			maxObj.deleteMax();
+			System.out.println(maxName + " "+ temporarymax);
 		}
 		System.out.println();
+		
 	}
+
 }
